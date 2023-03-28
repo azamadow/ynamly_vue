8bb1c13c1d4707ad2b5999de72174a58ff856d5d
azamadow <azamadow6@gmail.com>
2023-03-28 08:14:41 +0500

    deploy

---
+++ js/about.51b8d541.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdjackets"]=self["webpackChunkdjackets"]||[]).push([[443],{3258:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var e=u(3396);const c={class:"about"},r=(0,e._)("h1",null,"About",-1),s=[r];function a(n,t){return(0,e.wg)(),(0,e.iD)("div",c,s)}var o=u(89);const d={},f=(0,o.Z)(d,[["render",a]]);var i=f}}]);
+//# sourceMappingURL=about.51b8d541.js.map
